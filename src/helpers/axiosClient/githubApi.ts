796a71d99578730d4ce92f4ecc2f5f99061ff17c
Mirza Chilman <mirzachilman@gmail.com>
2023-07-19 22:58:05 +0700

    feat: add credentials

--- src/helpers/axiosClient/githubApi.ts
+++ src/helpers/axiosClient/githubApi.ts
@@ -5,8 +5,7 @@ const placeholderApi = axios.create({
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    Authorization:
-      "Bearer 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",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
   },
 });
 
