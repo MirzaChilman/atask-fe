5d431c4b3ccc9bf85e33ad6f9d1a4b16cdaf0e4f
Mirza <mirzachilman@gmail.com>
2023-07-24 09:50:34 +0700

    Update index.test.tsx

--- src/components/Repos/__tests__/index.test.tsx
+++ src/components/Repos/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "jotai";
-import Repos from "@/components/Repos"; // replace with your actual path
+import Repos from "@/components/Repos";
 
 describe("Repos Component", () => {
   it("renders without crashing", () => {
